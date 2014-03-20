99bdeb7038f1a65aae7d2d96daebb59a3a6668f2
stevepetebruce <stevepetebruce@hotmail.com>
2014-03-20 21:46:18 +0000

    tidied intro

--- books/book/js/chapterhead.js
+++ books/book/js/chapterhead.js
@@ -1,24 +1,73 @@
 // JavaScript Document
 
 
-function EasyPeasyParallax() {
-	scrollPos = $(this).scrollTop();
-	$('#banner').css({
-		'background-position' : '50% ' + (-scrollPos/4)+"px"
+//function EasyPeasyParallax() {
+//	scrollPos = $(this).scrollTop();
+	//$('#banner').css({
+	//	'background-position' : '50% ' + (-scrollPos/4)+"px"
+	//});
+	//$('#bannertext').css({
+	//	'margin-top': (scrollPos/4)+"px",
+	//	'opacity': 1-(scrollPos/250)
+	//});
+//}
+
+
+
+
+
+
+//$(document).ready(function(){
+//	$(window).scroll(function() {
+	//	EasyPeasyParallax();
+	//});
+//});
+
+
+
+
+
+
+
+
+//$w = $(window);
+//$square = $('#bannertext');
+
+//$w.scroll(function(){
+  //  $square.css('left',$w.scrollTop());
+//});
+
+
+
+
+
+
+
+(function(){
+	var ua = navigator.userAgent,
+		isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+
+	if (isMobileWebkit) {
+		$('html').addClass('mobile');
+	}
+
+	$(function(){
+		var iScrollInstance;
+
+		if (isMobileWebkit) {
+			iScrollInstance = new iScroll('wrapper');
+
+			$('#scroller').stellar({
+				scrollProperty: 'transform',
+				horizontalScrolling: false,
+				verticalOffset: 150
+			});
+		} else {
+			$.stellar({
+				horizontalScrolling: false,
+				verticalOffset: 150
+			});
+		}
 	});
-	$('#bannertext').css({
-		'margin-top': (scrollPos/4)+"px",
-		'opacity': 1-(scrollPos/250)
-	});
-}
 
-
-
-
-
-
-$(document).ready('touchmove',function(){
-	$(window).scroll(function() {
-		EasyPeasyParallax();
-	});
-});
+})();
